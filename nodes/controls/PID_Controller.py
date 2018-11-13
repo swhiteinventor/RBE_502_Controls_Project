5618ce005b08f5a151195f7ab6a1af77c2fd0231
swhiteinventor <swhiteinventor@gmail.com>
2018-11-13 12:50:40 -0500

    added functions for processing sub data and publishing twist data based off of v and theta

--- nodes/controls/PID_Controller.py
+++ nodes/controls/PID_Controller.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""Example Python node to listen & publish on a specific topic."""
-
 # Import required Python code.
 import roslib; roslib.load_manifest('controls')
 import rospy
@@ -13,37 +10,67 @@ from geometry_msgs.msg import Twist, TransformStamped
 
 class PID_Controller():
 
-    def __init__(self):
-        rospy.loginfo("Server node started.")
+	def __init__(self):
+		"""initializes the controls node"""
+		rospy.loginfo("Server node started.")
+	
+		#initializes the robot's position and orientation
+		self.x = 0
+		self.y = 0
+		self.z = 0
+		self.roll = 0
+		self.pitch = 0
+		self.yaw = 0
+	
+		#change wand to turtlebot later
+		rospy.Subscriber('/vicon/wand/wand', TransformStamped, self.on_data) 
+		self.pub = rospy.Publisher('topic/name/here', Twist, latch=True, queue_size=1)
 
-        rospy.Subscriber('/vicon/wand/wand', TransformStamped, self.on_data) #change wand to turtlebot later
+	def trajectory_tracking():
+		hi=1
 
-        self.pub = rospy.Publisher('topic/name/here', Twist, latch=True, queue_size=1)
-        t = Twist()
-        t.linear.x = 0.0 # TODO
-        t.linear.y = 0.0 # TODO
-        t.linear.z = 0
- 
-        t.angular.x = 0.0 # TODO
-        t.angular.y = 0.0 # TODO
-        t.angular.z = 0
-        self.pub.publish(t)
-        rospy.spin()
+	def send_twist_message(self, v, theta):
+		"""takes in the velocity and theta"""
+		t = Twist()
+		t.linear.x = v
+		t.linear.y = 0
+		t.linear.z = 0
+	 	t.angular.x = 0
+		t.angular.y = 0
+		t.angular.z = theta
+		self.pub.publish(t)
+		rospy.spin()
 
 
-    def on_data(self, data):
-            """Handle subscriber data."""
-            rospy.loginfo(rospy.get_name() + " I got data %s", data)
+	def on_data(self, data):
+		"""Callback function that handle subscriber data and updates self."""
+		rospy.loginfo(rospy.get_name() + " I got data %s", data)
+		#sets the position data	
+		self.x = data.linear.x
+		self.y = data.linear.y
+		self.z = data.linear.z
+		#converts the quaternion to euler angles
+		euler = quaternion_to_euler(data.angular)
+		#sets the orientation data	
+		self.roll = euler[0]
+		self.pitch = euler[1]
+		self.yaw = euler[2]
+
+	def quaternion_to_euler(quaternion):
+		"""converts a quaternion to euler angles"""
+		#type(pose) = geometry_msgs.msg.Pose
+		euler = tf.transformations.euler_from_quaternion(quaternion)
+		return euler
 
 # Main function.
 if __name__ == '__main__':
-    # Initialize the node and name it.
-    print "Initiating server node..."
-    rospy.init_node('PID_Controller')
+	# Initialize the node and name it.
+	print "Initiating server node..."
+	rospy.init_node('PID_Controller')
     
-    try:
-        rs = PID_Controller()
+	try:
+		rs = PID_Controller()
 
-    except rospy.ROSInterruptException:
-        rospy.logerror("Failed to start server node.")
-        pass
+	except rospy.ROSInterruptException:
+		rospy.logerror("Failed to start server node.")
+		pass
