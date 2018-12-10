d73ef6483328138117436598521a53d75b186af3
Davis Catherman <davis.catherman.14@cnu.edu>
2018-12-10 11:15:02 -0500

    post processing near complete

--- nodes/controls/post_processing.py
+++ nodes/controls/post_processing.py
@@ -13,7 +13,7 @@ class Post_Data():
 	def __init__(self):
 		return None
 
-def processor(bag_file='test.bag'):
+def processor(bag_file='test.bag', x_data_range=2):
 
 	start_time = 0
 	end_time   = 9E9
@@ -72,24 +72,42 @@ def processor(bag_file='test.bag'):
 	#print "  cmd_vel msgs="+str(  cmd_vel_msgs	)
 
 	#print "Process messages ..."
-	time_base = -1;
+	time_base = -1
+	startdada = 9E99
+	enddada = 9E99
 
 	if (vicon_msgs):
-		vicon_time = [0 for x in xrange(vicon_msgs)]
-		vicon_px    = [0 for x in xrange(vicon_msgs)]
-		vicon_py    = [0 for x in xrange(vicon_msgs)]
-		vicon_pz    = [0 for x in xrange(vicon_msgs)]
-		vicon_rx    = [0 for x in xrange(vicon_msgs)]
-		vicon_ry    = [0 for x in xrange(vicon_msgs)]
-		vicon_rz    = [0 for x in xrange(vicon_msgs)]
+		nodada = True
+
+		count = 0
+
+		for topic, msg, t0 in bag.read_messages(topics=vicon_topic):
+
+			if msg.transform.translation.x >= -x_data_range and nodada:
+				time_base = msg.header.stamp.to_sec()
+				startdada = (msg.header.stamp.to_sec() - time_base)
+				nodada = False
+
+			if nodada == False:
+				count += 1
+
+			if msg.transform.translation.x > x_data_range:
+				enddada = (msg.header.stamp.to_sec() - time_base)
+				break
+
+		vicon_time = [None for x in xrange(count)]
+		vicon_px    = [None for x in xrange(count)]
+		vicon_py    = [None for x in xrange(count)]
+		vicon_pz    = [None for x in xrange(count)]
+		vicon_rx    = [None for x in xrange(count)]
+		vicon_ry    = [None for x in xrange(count)]
+		vicon_rz    = [None for x in xrange(count)]
 
 		pt = 0
 
 		for topic, msg, t0 in bag.read_messages(topics=vicon_topic):
-				if (time_base < 0):
-					time_base = msg.header.stamp.to_sec()
-					#print "time base =",time_base
 
+			if (msg.header.stamp.to_sec() - time_base) >= startdada and (msg.header.stamp.to_sec() - time_base) <= enddada:
 				vicon_time[pt] = (msg.header.stamp.to_sec() - time_base)
 				vicon_px[pt]    = msg.transform.translation.x
 				vicon_py[pt]    = msg.transform.translation.y
@@ -113,20 +131,27 @@ def processor(bag_file='test.bag'):
 		#print "vicon end time ="+str(max(vicon_time))
 
 	if (cmd_vel_msgs):
-		cmd_time = [0 for x in xrange(cmd_vel_msgs)]
-		cmd_vx = [0 for x in xrange(cmd_vel_msgs)]
-		cmd_wz = [0 for x in xrange(cmd_vel_msgs)]
+		count = 0
+
+		for topic, msg, t0 in bag.read_messages(topics=cmd_vel_topic):
+			if (msg.header.stamp.to_sec() - time_base) >= startdada and (msg.header.stamp.to_sec() - time_base) <= enddada:
+				count += 1
+
+		cmd_time = [0 for x in xrange(count)]
+		cmd_vx = [0 for x in xrange(count)]
+		cmd_wz = [0 for x in xrange(count)]
 		# cmd vel
 
 		pt = 0
 
 		for topic, msg, t0 in bag.read_messages(topics=cmd_vel_topic):
-			cmd_time[pt] = (msg.header.stamp.to_sec() - time_base)
-			cmd_vx[pt]   = msg.twist.linear.x
-			cmd_vx[pt]   = msg.twist.linear.x
-			cmd_wz[pt]   = msg.twist.angular.z
+			if (msg.header.stamp.to_sec() - time_base) >= startdada and (msg.header.stamp.to_sec() - time_base) <= enddada:
+				cmd_time[pt] = (msg.header.stamp.to_sec() - time_base)
+				cmd_vx[pt]   = msg.twist.linear.x
+				cmd_vx[pt]   = msg.twist.linear.x
+				cmd_wz[pt]   = msg.twist.angular.z*180.0/pi
 
-			pt = pt + 1
+				pt = pt + 1
 
 		#print "Cmd start time ="+str(min(cmd_time))
 		#print "Cmd end time ="+str(max(cmd_time))
@@ -146,6 +171,28 @@ def processor(bag_file='test.bag'):
 		veloc.append(delta_d/delta_t - 0.325)
 		veloc_t.append(vicon_time[i])
 
+
+	v_error_rms = np.sqrt(np.mean(np.array(veloc)**2))
+	theta_error_rms = np.sqrt(np.mean(np.array(vicon_rz)**2))
+	print "v_error_rms,", v_error_rms
+	print "theta_error_rms,", theta_error_rms
+
+	v_temp = [abs(x) for x in veloc]
+	theta_temp = [abs(x) for x in vicon_rz]
+
+	v_error_max = max(v_temp)
+	theta_error_max = max(theta_temp)
+
+	print "v_error_max,", v_error_max
+	print "theta_error_max,", theta_error_max
+
+	v_error_avg = np.mean(np.array(veloc))
+	theta_error_avg = np.mean(np.array(vicon_rz))
+
+	file = open("outdata.txt","a") 
+	data_str = "%s, %s, %s, %s, %s, %s, %s,\n" % (bag_file, v_error_rms, theta_error_rms, v_error_max, theta_error_max, v_error_avg, theta_error_avg)
+	file.write(data_str)
+	file.close() 
 
 	pd = Post_Data()
 	pd.vicon_t = vicon_time
@@ -174,8 +221,10 @@ def min_max(array):
 
 def plotter(pd, cntrllr="PID", disturbance="board", trial="1", saver=""):
 
-	name = "%s controller with %s disturbance. Trial #%s: " % (cntrllr, disturbance, trial)
+	name = "%s Controller with %s Disturbance, Trial #%s: " % (cntrllr, disturbance, trial)
 	
+	grid_color = 'grey'
+	plot_lw = 1.5
 
 	#print len(pd.cmd_t), len(pd.vicon_t)
 	tmin, tmax = min_max([pd.vicon_t, pd.cmd_t])
@@ -186,16 +235,22 @@ def plotter(pd, cntrllr="PID", disturbance="board", trial="1", saver=""):
 
 	fig = plt.figure()
 	subplt = fig.add_subplot(111)
+	subplt.grid(color=grid_color, linestyle='-', linewidth=.5)
 	subplt.hold(True)
-	subplt.plot(pd.cmd_t,pd.cmd_vx,'r')
-	subplt.plot(pd.cmd_t,pd.cmd_wz,'g')
-	subplt.axis([tmin, tmax, pwmin, pwmax])
+	a = subplt.plot(pd.cmd_t,pd.cmd_vx,'r', linewidth=plot_lw)
+	subplt.set_ylabel('linear velocity (m/s)')
+	subplt.set_xlabel('time (s)')
+	#subplt.legend(['commanded linear velocity (m/s)'])
 
-	subplt.set_ylabel('m/s rad/s')
-	subplt.set_xlabel('time')
-	subplt.legend(['vx_cmd (m/s)', 'wz_cmd (rad/s)'])
+	subplt2 = subplt.twinx()	
+
+	b = subplt2.plot(pd.cmd_t,pd.cmd_wz,'b', linestyle='-', linewidth=plot_lw)
+	subplt2.legend(a+b,['commanded linear velocity (m/s)','commanded angular velocity (deg/s)'], fancybox=True, framealpha=0.5)
+	subplt2.axis([tmin, tmax, pwmin, pwmax])
+	subplt2.set_ylabel('angular velocity (deg/s)')
 	fig.suptitle(name + "Commands")
 	fig.savefig(saver + " Commands.png")
+	plt.close()
 
 	#print "  Plot vicon ..."
 
@@ -203,34 +258,38 @@ def plotter(pd, cntrllr="PID", disturbance="board", trial="1", saver=""):
 
 	fig = plt.figure()
 	subplt = fig.add_subplot(111)
+	subplt.grid(color=grid_color, linestyle='-', linewidth=.5)
 	subplt.hold(True)
-	subplt.plot(pd.vicon_t,pd.vicon_px,'r')
-	subplt.plot(pd.vicon_t,pd.vicon_py,'g')
+	subplt.plot(pd.vicon_t,pd.vicon_px,'r', linewidth=plot_lw)
+	subplt.plot(pd.vicon_t,pd.vicon_py,'g', linestyle='-', linewidth=plot_lw)
 	# subplt.plot(vicon_time,vicon_pz,'b')
+	subplt.legend(['global position x (m)', 'global position y (m)'], fancybox=True, framealpha=0.5)
 	subplt.axis([tmin, tmax, pmin, pmax])
 
-	subplt.set_ylabel('meters')
-	subplt.set_xlabel('time')
-	subplt.legend(['px_actual (m)', 'py_actual (m)'])
+	subplt.set_ylabel('position (m)')
+	subplt.set_xlabel('time (s)')
 	fig.suptitle(name + "Vicon Data")
 	fig.savefig(saver + " Vicon Data.png")
+	plt.close()
 
 
 	rmin, rmax = min_max([pd.vicon_rz])
 
 	fig = plt.figure()
 	subplt = fig.add_subplot(111)
+	subplt.grid(color=grid_color, linestyle='-', linewidth=.5)
 	subplt.hold(True)
 	# subplt.plot(vicon_time,vicon_rx,'r')
 	# subplt.plot(vicon_time,vicon_ry,'g')
-	subplt.plot(pd.vicon_t,pd.vicon_rz,'b')
+	subplt.plot(pd.vicon_t,pd.vicon_rz,'b', linewidth=plot_lw)
+	subplt.legend(['theta (yaw) error (deg)'], fancybox=True, framealpha=0.5)
 	subplt.axis([tmin, tmax, rmin, rmax])
 
-	subplt.set_ylabel('degrees')
-	subplt.set_xlabel('time')
-	subplt.legend(['rz_error (degrees)'])
+	subplt.set_ylabel('angle (deg)')
+	subplt.set_xlabel('time (s)')
 	fig.suptitle(name + "Angle Error")
 	fig.savefig(saver + " Angle Error.png")
+	plt.close()
 
 
 	#print "  Plot velocity ..."
@@ -240,16 +299,17 @@ def plotter(pd, cntrllr="PID", disturbance="board", trial="1", saver=""):
 
 	fig = plt.figure()
 	subplt = fig.add_subplot(111)
-	subplt.grid(color='black', linestyle='-', linewidth=1)
+	subplt.grid(color=grid_color, linestyle='-', linewidth=.5)
 	subplt.hold(True)
-	subplt.plot(pd.veloc_t,pd.veloc,'b')
+	subplt.plot(pd.veloc_t,pd.veloc,'r', linewidth=plot_lw)
 	# subplt.plot(vicon_time,vicon_pz,'b')
+	subplt.legend(['linear velocity error (m/s)'], fancybox=True, framealpha=0.5)
 	subplt.axis([vtmin, vtmax, vmin, vmax])
-	subplt.set_ylabel('meters/second')
-	subplt.set_xlabel('time')
-	subplt.legend(['velocity_error (m/s)'])
+	subplt.set_ylabel('linear velocity (m/s)')
+	subplt.set_xlabel('time (s)')
 	fig.suptitle(name + "Velocity Error")
 	fig.savefig(saver + " Velocity Error.png")
+	plt.close()
 
 def fix_data(bag_file):
 
@@ -285,15 +345,21 @@ def fix_data(bag_file):
 
 if __name__ == '__main__':
 	os.chdir(".")
-	for bag_file in glob.glob("NLF_board_2.bag"):
+	file = open("outdata.txt","w")
+	file.write("Filename, v_error_rms, theta_error_rms, v_error_max, theta_error_max, v_error_avg, theta_error_avg,\n")
+	file.close() 
+
+	for bag_file in glob.glob("PID_board_1.bag"):
+		if "fixed" in bag_file:
+			continue
 		print(bag_file)
 		a = bag_file[:-4].split('_')
-		cntrllr = a[0]
-		disturbance = a[1]
-		trial = a[2]
+		cntrllr = a[0].upper()
+		disturbance = a[1].title()
+		trial = a[2].title()
 
 		repaired_file = fix_data(bag_file)
 
-		pd = processor(repaired_file)
+		pd = processor(repaired_file, 1.5)
 		plotter(pd, cntrllr, disturbance, trial, bag_file[:-4])
 
